f1ad64fa0634730270b55d77c269c375f47660b3
ZaryonDE <Zaryon@gmx.de>
2025-09-23 20:50:14 +0200

    change something

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ tasks.named("build") {
 
 // -----------------------------
 base {
-    archivesBaseName = "NoHunger-1.20.1-Fabric"
+    archivesBaseName = "NoHunger-1.20-Fabric"
 }
 
 repositories {
@@ -56,12 +56,12 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.1")
-    mappings("net.fabricmc:yarn:1.20.1+build.1:v2")
+    minecraft("com.mojang:minecraft:1.20")
+    mappings("net.fabricmc:yarn:1.20+build.1:v2")
     modImplementation("net.fabricmc:fabric-loader:0.17.2")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.92.6+1.20.1")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.83.0+1.20")
     modImplementation("me.shedaniel.cloth:cloth-config-fabric:11.1.136")
-    modImplementation("com.terraformersmc:modmenu:7.2.2")
+    modImplementation("com.terraformersmc:modmenu:7.0.1")
 }
 
 java {
