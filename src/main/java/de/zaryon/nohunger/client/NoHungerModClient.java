21028b2f4662e995dc6e3975d787a218e8b9af4f
ZaryonDE <Zaryon@gmx.de>
2025-10-03 11:36:23 +0200

    change something

--- src/main/java/de/zaryon/nohunger/client/NoHungerModClient.java
+++ src/main/java/de/zaryon/nohunger/client/NoHungerModClient.java
@@ -22,10 +22,7 @@ public class NoHungerModClient implements ClientModInitializer, ModMenuApi {
         return parent -> NoHungerConfig.getInstance().createConfigScreen().setParentScreen(parent).build();
     }
     // Methode zur Bereitstellung der Beschreibung an ModMenu
-
     public Text getDescription() {
-
         return Text.translatable("mod.nohunger.description");
-
     }
 }
